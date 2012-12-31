54ad40bfb284745e5c27e8982b0c12da6a704d7d
mhdnuryo <mhdnuryo@gmail.com>
2013-01-01 00:40:26 +0700

    menambahkan support dari sisi server untuk fitur typing realtime pada sisi client

--- utils/handle-socket.js
+++ utils/handle-socket.js
@@ -3,6 +3,33 @@ function handleSocket(app,client){
   	console.log(roomId)
     client.join(roomId)
   })
+
+  client.on('typing',(user) => {
+    var [to,from] = parsing(
+      user
+    )
+
+    app.socket.to(to).emit(
+      'type',from
+    )
+  })
+
+  client.on('typed',(user) => {
+  	var [to,from] = parsing(
+      user
+  	)
+
+  	app.socket.to(to).emit(
+      'blur',from
+  	)
+  })
+}
+
+function parsing(param){
+  return [
+    param.uniqueId,
+    param.sender
+  ]
 }
 
 export {
