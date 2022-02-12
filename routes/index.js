7ec64f3d0b87a392e3c30b8e983a99a2037c6932
mhdnuryo <mhdnuryo@gmail.com>
2022-02-12 10:46:19 +0700

    mengubah response route index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',(req,res,next) => {
-  res.send('restricted, origin access : http://localhost:5000')
+  res.send('restricted, origin access : http://localhost:3000')
 })
 
 module.exports = router
