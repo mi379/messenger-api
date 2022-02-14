8ce2d73eaf80e0f9b53b01177be11cf365b8c560
mhdnuryo <mhdnuryo@gmail.com>
2022-02-14 13:05:23 +0700

    route index update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',(req,res,next) => {
-  res.send('restricted, origin access : http://localhost:3000')
+  res.send('missing origin acccess')
 })
 
 module.exports = router
