dafdebddf5c06786518565a7bc1ed69d9524e3c7
mhdnuryo <mhdnuryo@gmail.com>
2022-02-14 10:58:39 +0700

    origin access test

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize())
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({origin:['http://192.168.1.7:3000']}));
+app.use(express.urlencoded({extended:false}));
+app.use(cors({origin:['http://localhost:3000']}));
 app.use(express.static(path.join(__dirname,'public')));
 
 
